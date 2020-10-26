c10232d68d5aa9993771657b6e01e44af8a5124c
Joel <joelabq@gmail.com>
2020-10-26 08:07:28 -0600

    removed redundant array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var fetch = require('node-fetch')
 var pokeAPI = "https://pokeapi.co/api/v2/pokemon"
 let pokePromises = []
-var pokeTypes = []
+var thisPokemon = {}
 
 //get the pokemon names from the input file
 var pokemon = fs.readFileSync('input.txt').toString().split("\n")
@@ -19,9 +19,7 @@ async function pokeAPIFetcher(apiURL) {
                             Object.values(data.types).forEach( currentPokeData => {
                                      currentPokeTypes.push(`${currentPokeData.type.name}`)
                             })
-                        let thisPokemon = {}
                         thisPokemon[individualPokemon] =  currentPokeTypes.join(", ") 
-                        pokeTypes.push(thisPokemon)
                     }))
         })
 }
@@ -30,11 +28,11 @@ pokeAPIFetcher();
 
  Promise.all(pokePromises)
      .then(() => {
-        pokeTypes.reverse().forEach(thisPoke => {
-            for (const [poke, pokeType] of Object.entries(thisPoke)) {
+        
+            for (const [poke, pokeType] of Object.entries(thisPokemon)) {
                 console.log(`${poke}: ${pokeType}`)
             }
-        })
+        
         
         
     })
